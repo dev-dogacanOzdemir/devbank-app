34c2e83e55cad0fd9ba80eaeda182a451a870bde
d0a <dogacan.ozdemir.dev@gmail.com>
2024-12-13 18:00:48 +0300

    feat: reset modules master-13122024

--- accounting/account-api/src/main/java/com/devbank/account/api/enums/AccountType.java
+++
@@ -1,6 +0,0 @@
-package com.devbank.account.api.enums;
-
-public enum AccountType {
-    SAVINGS,   // Vadeli hesap
-    CURRENT    // Vadesiz hesap
-}
--- transaction-service/transaction-service-api/src/main/java/com/devbank/transaction/service/api/enums/TransactionType.java
+++
@@ -1,7 +0,0 @@
-package com.devbank.transaction.service.api.enums;
-
-public enum TransactionType {
-    DEPOSIT,    // Para yatırma
-    WITHDRAWAL, // Para çekme
-    TRANSFER    // Hesaplar arası transfer
-}
